4448be387e0e24a88ae1b1187c5d89977374d290
sergio <sergioricjr7@gmail.com>
2023-01-05 11:07:50 -0300

    adicionando multiplas paredes

--- combinacoes.py
+++ combinacoes.py
@@ -3,6 +3,8 @@
 indice1 = []
 indice2 = []
 
+# testar usar "is not in" ao inves de "not in"
+
 lp =[{'nome': 'pt1', 'largura': 3},{'nome': 'pt2', 'largura': 2},{'nome': 'pt3', 'largura': 2},{'nome': 'pt4', 'largura': 2.5},{'nome': 'pt5', 'largura': 3.5},{'nome': 'pt6', 'largura': 3.2}]
 # dicionario_paredes[0].pop('nome')
 # del dicionario_paredes[0]
@@ -17,26 +19,33 @@ paredes_carregadas = ['pt1']
 indice1 = []
 indice2 = []
 combinacoes = []
-for y in lp:    
+for y in lp:  
+    indice1 = []
+    indice2 = []
+    combinacoes = []  
     for i in range(len(lp)):
         if i==y:
             continue
-        elif lp[x]['nome'] not in paredes_carregadas and lp[i]['nome'] not in paredes_carregadas:
+        elif y['nome'] not in paredes_carregadas and lp[i]['nome'] not in paredes_carregadas:
             c2 = lp[x]['largura'] + lp[i]['largura']
             if c2 <= 6:
                 combinacoes.append(c2)
-                indice1.append(x)
+                indice1.append(lp.index(y))
                 indice2.append(i)
 
-    combinacoes_finais.append(
-        {
-            'nomes': [lp[indice1[combinacoes.index(max(combinacoes))]]['nome'], lp[indice2[combinacoes.index(max(combinacoes))]]['nome']],
-            'larguras': [lp[indice1[combinacoes.index(max(combinacoes))]]['largura'], lp[indice2[combinacoes.index(max(combinacoes))]]['largura']],
-            'total': [lp[indice1[combinacoes.index(max(combinacoes))]]['largura'] + lp[indice2[combinacoes.index(max(combinacoes))]]['largura']]
-        }
-    )
-    paredes_carregadas.append(lp[indice1[combinacoes.index(max(combinacoes))]]['nome'])
-    paredes_carregadas.append(lp[indice2[combinacoes.index(max(combinacoes))]]['nome'])
+    print(combinacoes, indice1, indice2, sep='\n')
+    print(max(combinacoes))
+    print(lp[indice1[combinacoes.index(max(combinacoes))]]['nome'])
+#print(lp[indice2[combinacoes.index(max(combinacoes))]]['nome'])
+    # combinacoes_finais.append(
+    #     {
+    #     'nomes': [lp[indice1[combinacoes.index(max(combinacoes))]]['nome'], lp[indice2[combinacoes.index(max(combinacoes))]]['nome']]
+    # #         'larguras': [lp[indice1[combinacoes.index(max(combinacoes))]]['largura'], lp[indice2[combinacoes.index(max(combinacoes))]]['largura']],
+    # #         'total': [lp[indice1[combinacoes.index(max(combinacoes))]]['largura'] + lp[indice2[combinacoes.index(max(combinacoes))]]['largura']]
+    #      }
+    # )
+    # # paredes_carregadas.append(lp[indice1[combinacoes.index(max(combinacoes))]]['nome'])
+    # paredes_carregadas.append(lp[indice2[combinacoes.index(max(combinacoes))]]['nome'])
 # if indice1[combinacoes.index(max(combinacoes))] > indice2[combinacoes.index(max(combinacoes))]:
 #     del lp[indice1[combinacoes.index(max(combinacoes))]]
 #     del lp[indice2[combinacoes.index(max(combinacoes))] + 1]
