dc0722bfd828f88b8c618d9ad51c8584bb6accf8
sergio <sergioricjr7@gmail.com>
2023-01-17 15:57:34 -0300

    otimzacao p achar melhores combinacoes de 2 paredes

--- testes.py
+++ testes.py
@@ -39,27 +39,47 @@ combinacoes_finais = []
 paredes_combinadas = []
 # paredes_combinadas1 = []
 # paredes_combinadas2 = []
-for x in lp:   # ERRO AO PULAR PAREDES NA LISTA DE PAREDES COMBINADAS, TENTER FAZER POR ITERACAO DE LISTA
-    nome1 = []
-    nome2 = []
-    combinacoes = []
+for x in lp:   # melhorar forma d fazer para escolher as melhores combinacoes
+    # nome1 = []
+    # nome2 = []
+    # combinacoes = []
     for i in lp:
         if x==i:
             continue
-        if i['nome'] in paredes_combinadas or x['nome'] in paredes_combinadas:
-            continue
+        # if i['nome'] in paredes_combinadas or x['nome'] in paredes_combinadas:
+        #     continue
         else:
             c2 = x['largura'] + i['largura']
             if c2 <= 6:
                 combinacoes.append(c2)
                 nome1.append(x['nome'])
                 nome2.append(i['nome'])
-    try:
-        indice = combinacoes.index(max(combinacoes))
-        paredes_combinadas.append(nome1[indice])
-        paredes_combinadas.append(nome2[indice])
-    except:
-        continue
+for i in range(len(combinacoes)): #jeito certo de achar as melhores combinacoes
+    indice = combinacoes.index(max(combinacoes))
+    noome1 = nome1[indice]
+    noome2 = nome2[indice]
+    soma = max(combinacoes)
+    if noome1 in paredes_combinadas or noome2 in paredes_combinadas:
+        combinacoes.pop(indice)
+        nome1.pop(indice)
+        nome2.pop(indice)       
+    else:
+        try:
+            paredes_combinadas.append(nome1[indice])
+            paredes_combinadas.append(nome2[indice])
+            combinacoes_finais.append((nome1[indice], nome2[indice]))
+            combinacoes.pop(indice) 
+            nome1.pop(indice)
+            nome2.pop(indice)   
+        except:
+            continue
+    # try:
+    #     indice = combinacoes.index(max(combinacoes))
+    #     paredes_combinadas.append(nome1[indice])
+    #     paredes_combinadas.append(nome2[indice])
+    #     combinacoes_finais.append((nome1[indice], nome2[indice]))
+    # except:
+    #     continue
     # print(nome1)
     # print(nome2)
     # try:
@@ -77,4 +97,4 @@ for x in lp:   # ERRO AO PULAR PAREDES NA LISTA DE PAREDES COMBINADAS, TENTER FA
 print(combinacoes, nome1, nome2, sep='\n')
 for i in combinacoes:  #tentar fazer as combinacoes de vdd fora do primeiro loop
     pass
-print(paredes_combinadas)
+print(combinacoes_finais)
